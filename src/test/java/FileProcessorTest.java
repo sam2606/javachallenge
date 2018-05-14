068dc88c1feeb3d939271e6056a59201842d3d73
sameer.gaherwar <sameer.gaherwar@fiserv.com>
2018-05-14 19:07:33 -0400

    Specify the file path

--- src/test/java/FileProcessorTest.java
+++ src/test/java/FileProcessorTest.java
@@ -3,7 +3,7 @@ import com.demo.util.FileProcessor;
 public class FileProcessorTest {
 
     public static void main(String[] args) {
-        FileProcessor process = new FileProcessor("/Users/sameer.gaherwar/Desktop/test");
+        FileProcessor process = new FileProcessor("path to file");
         process.printWordCounts();
     }
 }
